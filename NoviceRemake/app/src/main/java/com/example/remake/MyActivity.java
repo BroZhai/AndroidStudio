d32029b7e9d975ba46aa9239dac2beb15ca3ccdf
Working Tech <k2785233247@gmail.com>
2024-09-27 12:42:10 +0800

    学会了'输入框'EditText组件的使用，并用一个单独的EditText类对象来 找到存储其输入内容 (和'找按钮'的过程很像)，存储到EditText对象后，再用.getText().toString()转成字符串就可以用啦，这顿下来同时了解了下UI内'简单的约束'布局设置

--- NoviceRemake/app/src/main/java/com/example/remake/MyActivity.java
+++ NoviceRemake/app/src/main/java/com/example/remake/MyActivity.java
@@ -3,6 +3,7 @@ package com.example.remake;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MyActivity extends AppCompatActivity {
 
+    private EditText iText; // 声明一个EditText对象，准备用于接收<ExitText>控件的内容
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,22 @@ public class MyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MyActivity.this,"按钮被点击啦！",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // 下面是那个"submit"的按钮
+        Button submitBtn=(Button) findViewById(R.id.submitText);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 通过id找到对应的EditText控件，并存储到本地的EditText 对象iText中
+                iText=(EditText)findViewById(R.id.inputText);
+                // iText对象里面存的东西，我们还需要.getText().toString()之后才能转成Java里面的String
+                String textcontent=iText.getText().toString();
+
+                // 最后在Toast中悬浮展示
+                Toast.makeText(MyActivity.this,"Hello "+textcontent +"!",Toast.LENGTH_LONG).show();
             }
         });
     }
