c5d608d1445812c64c620f75b193fa9cde5f1f45
Working Tech <k2785233247@gmail.com>
2024-10-18 16:37:23 +0800

    用模版添加了第一个Activity

---
+++ RetakeComponents/app/src/main/java/com/example/retakecomponents/firstActivity.java
@@ -0,0 +1,24 @@
+package com.example.retakecomponents;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class firstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.first_layout); // 这里是模版自动生成的"引用布局"，省得自己再去引用了
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
