3eb21bbbd14169627aa546b3dd04c311f464b804
Working Tech <k2785233247@gmail.com>
2024-10-19 19:01:30 +0800

    完成了多选控件Checkbox的实践，同时简单修正了下SecondActivity的样式

--- RetakeComponents/app/src/main/java/com/example/retakecomponents/SecondActivity.java
+++ RetakeComponents/app/src/main/java/com/example/retakecomponents/SecondActivity.java
@@ -2,15 +2,22 @@ package com.example.retakecomponents;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -59,6 +66,55 @@ public class SecondActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // 获取所有的多选按钮
+        CheckBox fire = findViewById(R.id.flareon);
+        CheckBox water = findViewById(R.id.vaporeon);
+        CheckBox leaf = findViewById(R.id.leafeon);
+        CheckBox fairy = findViewById(R.id.sylveon);
+        CheckBox ice = findViewById(R.id.glaceon);
+        CheckBox moon = findViewById(R.id.umbreon);
+        CheckBox sun = findViewById(R.id.espeon);
+
+        Button checkEevee = findViewById(R.id.checkEevee); // 获取"检查按钮"
+
+        // 为"检查按钮"设置监听器
+        checkEevee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> eeveePack = new ArrayList<>();
+                TextView eeveeResult = findViewById(R.id.eeveeDisplay);
+                if (fire.isChecked()){
+                    eeveePack.add(fire.getText().toString());
+                }
+                if (water.isChecked()){
+                    eeveePack.add(water.getText().toString());
+                }
+                if (leaf.isChecked()){
+                    eeveePack.add(leaf.getText().toString());
+                }
+                if (fairy.isChecked()){
+                    eeveePack.add(fairy.getText().toString());
+                }
+                if (ice.isChecked()){
+                    eeveePack.add(ice.getText().toString());
+                }
+                if (moon.isChecked()){
+                    eeveePack.add(moon.getText().toString());
+                }
+                if (sun.isChecked()){
+                    eeveePack.add(sun.getText().toString());
+                }
+
+                if(eeveePack.isEmpty()){
+                    Toast.makeText(SecondActivity.this,"我的布布们呢QAQ...",Toast.LENGTH_LONG);
+                    return;
+                }
+            eeveeResult.setText("你选择了: "+eeveePack.toString());
+            }
+        });
+
+
     }
 
     public void jumpBack(View view){
