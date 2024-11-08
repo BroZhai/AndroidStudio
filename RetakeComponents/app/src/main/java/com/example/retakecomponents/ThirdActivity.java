225255d2b9f596efab66f75453385863daa4ad30
Working Tech <k2785233247@gmail.com>
2024-11-08 23:31:52 +0800

    追加了优化listView的方法，等待搞明白setTag()和getTag()的用法，今天先到这里xwx...

--- RetakeComponents/app/src/main/java/com/example/retakecomponents/ThirdActivity.java
+++ RetakeComponents/app/src/main/java/com/example/retakecomponents/ThirdActivity.java
@@ -12,12 +12,15 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
 
 public class ThirdActivity extends Activity {
 
     private String[] titles = {"Mp3","电子词典","GPRS通信设备","mp4","台式电脑","GPS定位仪","声呐雷达","核能手电"};
     private String[] prices = {"100元","80元","230元","120元","2300元","333元","9999元","Undefinded"};
-
     protected void onCreate(Bundle savedInstanceState){
         // 复习一下'新建Activity'，好久没动了QAQ...
         // 准备在这里实现一下'listView' + 'Adapter'的内容
@@ -29,6 +32,7 @@ public class ThirdActivity extends Activity {
         MyAdapter createdAdapter = new MyAdapter();
         lv.setAdapter(createdAdapter);
     }
+
 
 
 
@@ -57,9 +61,10 @@ public class ThirdActivity extends Activity {
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
+        /*public View getView(int i, View view, ViewGroup viewGroup) {
             //重点: 获取每一个item的'展示样式' (单独开个layout文件设置每个Item的样式)
-            //参数说明: i (item的下标位置)， viewGroup: 待查明...
+            //参数说明: i (item的下标位置)， view (缓存着'划出视野'的item, 优化listView用), viewGroup(listView对象本身)
+
 
             //1. 使用View.inflate()加载布局 inflate(Activity对象，布局文件，不知道就填null);
             View item_view = view.inflate(ThirdActivity.this, R.layout.item_layout, null);
@@ -76,10 +81,36 @@ public class ThirdActivity extends Activity {
 
             //4. 返回该View对象 (这样以后，这个Adapter就算建立好了，接下来将这个"格式"应用到listView控件中去)
             return item_view;
+        }*/
+
+        //EXTRA: 创建一个viewHolder类，这个类专门对划出屏幕的item进行缓存，从而优化ListView性能
+        // TODO: 搞明白这里view对象的setTag()和getTag()方法
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            MyViewHolder holder;
+            if(view == null){
+                view = view.inflate(ThirdActivity.this, R.layout.item_layout, null);
+                holder = new MyViewHolder();
+                holder.name = view.findViewById(R.id.item_title);
+                holder.price = view.findViewById(R.id.item_price);
+                view.setTag(holder);
+            }else{
+                holder = (MyViewHolder) view.getTag();
+            }
+
+            //3. 根据"原数据"设置各个控件的'展示数据'
+            holder.name.setText(titles[i]);
+            holder.price.setText(prices[i]);
+
+            //4. 返回该View对象 (这样以后，这个Adapter就算建立好了，接下来将这个"格式"应用到listView控件中去)
+            return view;
+        }
+        class MyViewHolder{
+            TextView name;
+            TextView price;
         }
     }
 
-    // 跳会Activity2的方法
+    // 跳回Activity2的方法
     public void jumpBack(View view){
         finish();
     }
