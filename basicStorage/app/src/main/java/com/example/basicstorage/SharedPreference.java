fe54eddded847ca27d13e221865829ccf40b8138
Working Tech <k2785233247@gmail.com>
2024-11-12 00:50:59 +0800

    实现了SharedPreferences便利键值对KV存储，相当于一个'友好版'的JS,但用的是'内部存储',卸载程序数据即消失

--- basicStorage/app/src/main/java/com/example/basicstorage/SharedPreference.java
+++ basicStorage/app/src/main/java/com/example/basicstorage/SharedPreference.java
@@ -1,7 +1,10 @@
 package com.example.basicstorage;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,5 +30,37 @@ public class SharedPreference extends AppCompatActivity {
     public void goOuterStorage(View view) {
         // 跳回OuterStorage
         finish();
+    }
+
+
+    public void SPWrite(View view) {
+        // 创建SharedPreferences对象，用方法getSharedPreferences("存储文件名", 模式[一般用MODE_PRIVATE])
+        // MODE_PRIVATE: 只有当前应用程序才能访问这个SharedPreferences文件
+        SharedPreferences sp = getSharedPreferences("userdata", MODE_PRIVATE);
+
+        // 获取SharedPreferences.Editor'专用编辑对象'，用于'编辑'(写入)数据
+        SharedPreferences.Editor editor = sp.edit();
+
+        // 通过editor对象写入数据 [editor对象.put各数据类型("键名",值);]
+        editor.putString("username", "admin");
+        editor.putInt("phone", 123456);
+
+        // 保存'写入的'数据
+        editor.apply();
+        Log.d("TAG","已成功向SharedPreferences写入数据");
+        Toast.makeText(SharedPreference.this,"写入成功, 请去LogCat查看",Toast.LENGTH_SHORT).show();
+    }
+
+    public void SPRead(View view) {
+        // 和写数据一样，先获取SharedPreferences对象
+        SharedPreferences sp = getSharedPreferences("userdata", MODE_PRIVATE);
+
+        // 现在可以直接通过这个SharedPreferences对象.get各数据类型("键名",没读到返回的默认值); 读取数据
+        String username = sp.getString("username", "null"); // 读取键名为"username"的数据，如果没有则返回"null"
+        int phone = sp.getInt("phone", 0); // 读取键名为"phone"的数据，如果没有则返回0
+
+        // 读到数据之后，该干啥干啥 (这里选择了在控制台打印)
+        Log.d("TAG","用户名:"+username+" 电话:"+phone);
+        Toast.makeText(SharedPreference.this,"已在LogCat打印控制台信息",Toast.LENGTH_SHORT).show();
     }
 }
